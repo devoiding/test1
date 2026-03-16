f9de3db23e40ad3db206d531fa32788289606207
devoiding <qiorgpap@gmail.com>
2026-03-16 18:18:12 +0200

    Class dummy

--- main.cpp
+++ main.cpp
@@ -2,8 +2,38 @@
 
 using namespace std;
 
-int main(void) { 
-    cout << "Hello World" << endl;
-    return 0;
+class dummy {
+        int x;
+    public: 
+        dummy();
+        dummy(int in_x);
+        ~dummy();
+        int get_x() const;
+        void set_x(int in_x);
+};
+
+int main(void) {
+    dummy d1, d2(10);
+    cout << "d1.x = " << d1.get_x() << endl;
+    cout << "d2.x = " << d2.get_x() << endl;
 }
 
+dummy::dummy() {
+    x = 0;
+}
+
+dummy::dummy(int in_x) {
+    x = in_x;
+}
+
+dummy::~dummy() {
+    cout << "The destructor is called" << endl;
+}
+
+int dummy::get_x() const {
+    return x;
+}
+
+void dummy::set_x(int in_x) {
+    x = in_x;
+}
