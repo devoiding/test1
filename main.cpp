7427b7b64cc940216699170fc7d03c005fe34305
devoiding <qiorgpap@gmail.com>
2026-03-16 18:58:31 +0200

    Gandalf

--- main.cpp
+++ main.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-class point {
-    int x, y;
+class Gandalf {
+    int magicalPower;
+    int age;
+    char beardColor[12];
 public:
-    point();
-    point(int in_x, int in_y);
+    Gandalf();
+    void mp_usage(int mp);
+    int get_mp() const;
+    void spell_fireball();
+    void spell_lightning();
+    void action_wait();
 
-    void print() const;
-    void set_xy(int in_x, int in_y);
 };
 
 int main(void) {
-    point p1, p2(3,4), p3(5,6);
-    p1.print();
-    p2.print();
-    p3.print();
+    Gandalf gandalf;
+    gandalf.spell_fireball();
+    gandalf.action_wait();
+    gandalf.spell_lightning();
+    gandalf.action_wait();
+    gandalf.action_wait();
+    gandalf.action_wait();
+    gandalf.spell_lightning();
+    gandalf.action_wait();
+    gandalf.action_wait();
+    gandalf.spell_fireball();
 }
 
-point::point() {
-    x = 0;
-    y = 0;
+Gandalf::Gandalf() {
+    magicalPower = 100;
+    age = 2019;
+    strcpy(beardColor, "gray");
+    cout << "Gandalf the Gray is born with magical power: " << magicalPower << endl;
 }
 
-point::point(int in_x, int in_y) {
-    x = in_x;
-    y = in_y;
+void Gandalf::mp_usage(int mp) {
+    magicalPower -= mp;
 }
 
-void point::print() const {
-    cout << "(" << x << ", " << y << ")" << endl;
+int Gandalf::get_mp() const {
+    return magicalPower;
 }
 
-void point::set_xy(int in_x, int in_y) {
-    x = in_x;
-    y = in_y;
+void Gandalf::spell_fireball() {
+    if (magicalPower > 30) {
+        cout << "Gandalf casts Fireball!" << endl;
+        mp_usage(30);
+    } else {
+        cout << "Not enough magical power for Fireball!" << endl;
+        cout << "Current magical power: " << magicalPower << endl;
+    }
 }
+
+void Gandalf::spell_lightning() {
+    if (magicalPower > 90) {
+        cout << "Gandalf casts Lightning!" << endl;
+        mp_usage(90);
+    } else {
+        cout << "Not enough magical power for Lightning!" << endl;
+    }
+    cout << "Current magical power: " << magicalPower << endl;
+}
+
+void Gandalf::action_wait() {
+    cout << "Gandalf waits and regain some magical power." << endl;
+    magicalPower += 10;
+    if (magicalPower > 100) {
+        magicalPower = 100;
+    }
+    cout << "Current magical power: " << magicalPower << endl;
+}
+
